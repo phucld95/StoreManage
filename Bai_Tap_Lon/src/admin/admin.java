a9789c9f30e4edc6a0dc6e1b6da160f6cf4546f6
tranlinh265 <tranlinh265@gmail.com>
2015-10-11 20:21:27 +0700

    xem ho t class login.java

---
+++ Bai_Tap_Lon/src/admin/admin.java
@@ -0,0 +1,16 @@
+package admin;
+
+import java.sql.*;
+import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class admin {
+	private static String username ="admin";
+	private static String password ="admin123";
+	
+	public static void nhap(){
+		
+	}
+	
+}
---
+++ Bai_Tap_Lon/src/support/hamnhap.java
@@ -0,0 +1,12 @@
+package support;
+import java.util.*;
+public class hamnhap {
+	public static String nhapchu(){
+		Scanner nhap=new Scanner(System.in);
+		return nhap.nextLine();
+	}
+	public static int nhapso(){
+		Scanner nhap = new Scanner(System.in);
+		return nhap.nextInt();
+	}
+}
