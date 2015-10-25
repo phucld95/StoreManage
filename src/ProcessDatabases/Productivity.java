1adab79b2656899a4ac7d03f0e5fd77018f8e852
nguyentientruong <nguyentientruong95@gmail.com>
2015-10-25 20:25:22 +0700

    hoàn thành chức năng hóa đơn

--- src/ProcessDatabases/Productivity.java
+++ src/ProcessDatabases/Productivity.java
@@ -7,43 +7,69 @@ import java.util.Scanner;
 
 public class Productivity {
 	static Statement st;
-	String sql= new String();
-	int sum,i;
+	String sql = new String();
+	int sum, i;
 	ResultSet rs;
 	Scanner Input = new Scanner(System.in);
-	public Productivity(Statement sts){
-		st=sts;
+
+	public Productivity(Statement sts) {
+		st = sts;
 	}
+
 	// tinh tien trung binh trong dot khuyen mai
-	public int AvgEvent(int id)throws SQLException{
+	public double AvgEvent(int id) throws SQLException {
 		String TGDR = new String();
 		String TGKT = new String();
-		sql=String.format("select TGDR,TGKT from khuyen_mai where Id_KM=%d",id);
-		rs=st.executeQuery(sql);
+		String result[] = new String[100];
+		sum = 0;
+		i = 0;
+		sql = String.format("select TGDR,TGKT from khuyen_mai where Id_KM=%d", id);
+		rs = st.executeQuery(sql);
 		rs.next();
-		TGDR=rs.getString("TGDR").concat("000000");
-		TGKT=rs.getString("TGKT").concat("235959");
-		sum=i=0;
-		//time[0].concat("000000");
-		//time[1].concat("235959");
-		sql=String.format("select Tong_Tien from hoa_don where %s <= Thoi_Gian and %s >=TGKT", TGDR,TGKT);
-				//SQL = String.format("select * from khuyen_mai where TGDR <= '%s' and TGKT >= '%s';", time.Date(), time.Date());
-		rs=st.executeQuery(sql);
-		while(!rs.next()){
-			sum=sum+rs.getInt("Tong_Tien");
+		result = rs.getString("TGDR").split("[-]");
+		for (String s : result) {
+			TGDR = TGDR.concat(s);
+		}
+		result = rs.getString("TGKT").split("[-]");
+		for (String r : result) {
+			TGKT = TGKT.concat(r);
+		}
+		TGDR = TGDR.concat("000000");
+		TGKT = TGKT.concat("235959");
+		// System.out.println(TGDR+" "+TGKT);
+		// tinh so tien trung binh trong dot khuyen mai
+		sql = String.format("select Tong_Tien from hoa_don where %s <= Thoi_Gian and %s >=Thoi_Gian", TGDR, TGKT);
+		rs = st.executeQuery(sql);
+		while (rs.next()) {
+			sum = sum + rs.getInt("Tong_Tien");
 			i++;
 		}
-		return sum=sum/i;
+		try {
+			return (sum / i);
+		} catch (Exception ex) {
+			return 0;
+		}
 	}
-	// ting tien trung binh cua 1 nhan vien ban duoc trong 1 khoang thoi nhan nhat dinh !
-	public int AvgMember(String timeStart , String timeEnd,int id_Account)throws SQLException{
-		sum=i=0;
-		sql=String.format("select Tong_Tien from hoa_don where ID_ThuNgan=%d and Thoi_Gian >=%s and Thoi_Gian <=%s", id_Account,timeStart.concat("000000"),timeEnd.concat("235959"));
-		rs=st.executeQuery(sql);
-		while(!rs.next()){
-			sum=sum+rs.getInt("Tong_Tien");
+
+	// ting tien trung binh cua 1 nhan vien ban duoc trong 1 khoang thoi gian
+	// nhat dinh !
+	public double AvgMember(String timeStart, String timeEnd, int id_Account) throws SQLException {
+		sum = i = 0;
+		timeStart = timeStart.concat("000000");
+		timeEnd = timeEnd.concat("235959");
+		// System.out.println(timeStart+" "+timeEnd);
+		sql = String.format(
+				"select Tong_Tien from hoa_don where ID_ThuNgan=%d and Thoi_Gian >='%s' and Thoi_Gian <='%s'",
+				id_Account, timeStart, timeEnd);
+		rs = st.executeQuery(sql);
+		while (rs.next()) {
+			sum = sum + rs.getInt("Tong_Tien");
 			i++;
 		}
-		return sum/i;
+		try {
+			return sum / i;
+		} catch (Exception ex) {
+			return 0;
+		}
 	}
 }
